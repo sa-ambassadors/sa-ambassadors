7a2a4fb0f78a09ecad750b19320a7f07b780dcd0
Cory Musick <cory.musick37@gmail.com>
2019-05-16 10:44:06 -0500

    took out old import

--- src/main/java/io/internhub/application/controllers/JobsController.java
+++ src/main/java/io/internhub/application/controllers/JobsController.java
@@ -3,7 +3,6 @@ import io.internhub.application.repositories.Jobs;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import static com.sun.tools.doclets.formats.html.markup.HtmlStyle.description;
 
 
 @Controller
