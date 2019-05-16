f248c7ab59bcc1bb7744da58caa85ab5f50e7bef
Cory Musick <cory.musick37@gmail.com>
2019-05-16 13:37:45 -0500

    intern registration page adds to db and redirects to login

---
+++ src/main/java/io/internhub/application/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package io.internhub.application.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+        @GetMapping("/")
+        private String index(){
+            return "index";
+    }
+
+}
