45ce8bcef3cd4fb2c95279bd4d612cf901fb20e7
michael.flores3717@gmail.com <mikeflores@Mikes-MacBook-Air.local>
2019-05-16 13:48:39 -0500

    added relationship between employers and jobs

--- src/main/java/io/internhub/application/controllers/JobsController.java
+++ src/main/java/io/internhub/application/controllers/JobsController.java
@@ -3,16 +3,15 @@ import io.internhub.application.repositories.Jobs;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import static com.sun.tools.doclets.formats.html.markup.HtmlStyle.description;
-
 
 @Controller
 public class JobsController {
     private Jobs jobs;
 
     public JobsController(Jobs jobs) {
-
+        this.jobs = jobs;
     }
+
 
 
 
