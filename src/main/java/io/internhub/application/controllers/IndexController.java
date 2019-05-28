2a1e2fac62415fb90f89db7d48fbf407501ab7a9
michael.flores3717@gmail.com <mikeflores@Mikes-MacBook-Air.local>
2019-05-27 20:26:55 -0500

    admin can now authorize users

--- src/main/java/io/internhub/application/controllers/IndexController.java
+++ src/main/java/io/internhub/application/controllers/IndexController.java
@@ -1,18 +1,16 @@
 package io.internhub.application.controllers;
 
-import io.internhub.application.models.User;
-import io.internhub.application.models.UserWithRoles;
-import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class IndexController {
 
 
         @GetMapping("/")
-        public String index(){
+        public String index() {
             return "index";
     }
 
