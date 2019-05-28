73dae72156d4e0df60ba09c5323e88b72f2d7376
michael.flores3717@gmail.com <mikeflores@Mikes-MacBook-Air.local>
2019-05-28 11:57:22 -0500

    adding bug branch

--- src/main/resources/static/js/profile-script.js
+++ src/main/resources/static/js/profile-script.js
@@ -2,11 +2,37 @@
 
     "use script";
 
-    console.log("test");
+    $('.edit-profile').hide();
+    $('.edit-emp-profile').hide();
 
-    document.querySelector("#save").addEventListener("click", saveInternProfile);
 
     let username = document.querySelector("#username").innerHTML;
+    if (document.querySelector("#save") != null) {
+        document.querySelector("#save").addEventListener("click", saveInternProfile);
+    }
+    if (document.querySelector("#save-emp") != null) {
+        document.querySelector("#save-emp").addEventListener("click", saveEmployerProfile);
+    }
+
+    $('#edit').click(function(event) {
+        event.preventDefault();
+        $('.show-profile').hide();
+        $('.edit-profile').show();
+    });
+
+    $('#edit-emp').click(function(event) {
+        console.log("test");
+        event.preventDefault();
+        $('.show-emp-profile').hide();
+        $('.edit-emp-profile').show();
+    });
+
+    // $('#save').click(function(event) {
+    //     event.preventDefault();
+    //     $('.edit-profile').hide();
+    //     $('.show-profile').show();
+    //
+    // });
 
     //Intern profile fields
     let firstName = null;
@@ -32,9 +58,9 @@
 
     function saveInternProfile(event) {
         event.preventDefault();
+        console.log("hello");
         setInternVars();
-        console.log(firstName);
-        console.log("test");
+        console.log(document.querySelector("#firstName").innerHTML);
         fetch("/api/v1/" + username + "/intern/profile/edit", {
             method: 'PATCH',
             headers: { 'Content-Type': 'application/json' },
@@ -43,15 +69,14 @@
                 "last_name": lastName,
                 "major": major,
                 "minor": minor,
-                "field_1": field1,
-                "field_2": field2,
-                "field_3": field3,
                 "transcript_link": transcriptLink,
                 "essay_link": essayLink,
                 "sa_highschool": saHighSchool,
                 "first_to_college": firstToCollege
             })
-        })
+        });
+        $('.edit-profile').hide();
+        $('.show-profile').show();
     }
 
     function saveEmployerProfile(event) {
@@ -70,25 +95,44 @@
                 "description": description
             })
         })
+        $('.edit-emp-profile').hide();
+        $('.show-emp-profile').show();
 
     }
 
     function setInternVars() {
-        firstName = document.querySelector("#firstName").innerHTML;
-        lastName = document.querySelector("#lastName").innerHTML;
-        major = document.querySelector("#major").innerHTML;
-        minor = document.querySelector("#minor").innerHTML;
-        field1 = document.querySelector("#field_1").innerHTML;
-        field2 = document.querySelector("#field_2").innerHTML;
-        field3 = document.querySelector("#field_3").innerHTML;
-        transcriptLink = document.querySelector("#transcript_link").innerHTML;
-        essayLink = document.querySelector("#essay_link").innerHTML;
-        saHighSchool = document.querySelector("#sa_highschool").innerHTML;
-        document.querySelector("#sa_college").innerHTML;
-        firstToCollege = document.querySelector("#first_to_college").innerHTML;
+        firstName = document.querySelector("#firstName").value;
+        lastName = document.querySelector("#lastName").value;
+        major = document.querySelector("#major").value;
+        minor = document.querySelector("#minor").value;
+        // console.log(document.querySelector("#field_1").value);
+        // field1 = document.querySelector("#field_1").value;
+        // field2 = document.querySelector("#field_2").value;
+        // field3 = document.querySelector("#field_3").value;
+
+        transcriptLink = document.querySelector("#transcript-field").value;
+        essayLink = document.querySelector("#essay-field").value;
+        console.log(document.querySelector("#sa_highschool").value);
+        if ((document.querySelector("#sa_highschool").value) === "false") {
+            saHighSchool = 0;
+        } else {
+            saHighSchool = 1;
+        }
+        if (document.querySelector("#first_to_college").value === "false") {
+            firstToCollege = 0;
+        } else {
+            firstToCollege = 1;
+        };
     }
 
     function setEmployerVars() {
+        empName = document.querySelector("#emp-name").value;
+        supervisor = document.querySelector("#supervisor").value;
+        location = document.querySelector("#location").value;
+        industry = document.querySelector("#industry").value;
+        websiteURL = document.querySelector("#website-url").value;
+        description = document.querySelector("#description").value;
     }
 
 })();
+
